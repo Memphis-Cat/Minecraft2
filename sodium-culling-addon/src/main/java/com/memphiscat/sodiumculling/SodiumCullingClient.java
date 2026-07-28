e9316f07ebce23e13750729db662ebeac9935074
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-07-28 19:04:32 +0100

    Add client initializer

---
+++ sodium-culling-addon/src/main/java/com/memphiscat/sodiumculling/SodiumCullingClient.java
@@ -0,0 +1,18 @@
+package com.memphiscat.sodiumculling;
+
+import com.memphiscat.sodiumculling.config.CullingConfig;
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class SodiumCullingClient implements ClientModInitializer {
+    public static final String MOD_ID = "sodiumculling";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static CullingConfig CONFIG;
+
+    @Override
+    public void onInitializeClient() {
+        CONFIG = CullingConfig.load();
+        LOGGER.info("Sodium Culling Addon initialized with conservative compatibility settings");
+    }
+}
