1b7985779d8f1baeb690f00abc28ca3431e700fa
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-07-28 21:45:07 +0100

    Load Legacy Culling configuration at startup

--- legacy-culling-1.8.9/src/main/java/com/memphiscat/legacyculling/LegacyCullingMod.java
+++ legacy-culling-1.8.9/src/main/java/com/memphiscat/legacyculling/LegacyCullingMod.java
@@ -1,5 +1,6 @@
 package com.memphiscat.legacyculling;
 
+import com.memphiscat.legacyculling.config.LegacyCullingConfig;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 public final class LegacyCullingMod implements ModInitializer {
     public static final String MOD_ID = "legacyculling";
     public static final Logger LOGGER = LogManager.getLogger("Legacy Culling");
+    public static final LegacyCullingConfig CONFIG = LegacyCullingConfig.load();
 
     @Override
     public void onInitialize() {
